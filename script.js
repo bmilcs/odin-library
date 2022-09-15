c125119efa9b178a62cccbf51838a3c07b567956
Bryan Miller <bmilcs@yahoo.com>
2022-09-15 16:33:05 -0400

    Add book constructor, add book to library & display all book functions

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log("Bryan's Library Project");
+let myLibrary = [];
+
+// constructor function
+function Book(title, author, pages, readStatus) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.readStatus = readStatus;
+}
+
+function addBookToLibrary(title, author, pages, readStatus) {
+  myLibrary.push(new Book(title, author, pages, readStatus));
+}
+
+function displayAllBooks() {
+  // Loop through array & display each book on the page
+  myLibrary.forEach((book) => {
+    let card = document.createElement("div"),
+      cardTitle = document.createElement("h2"),
+      cardAuthor = document.createElement("p"),
+      cardPages = document.createElement("p"),
+      cardReadStatus = document.createElement("p");
+
+    card.classList.add("card");
+
+    cardTitle.textContent = book.title;
+    cardAuthor.textContent = book.author;
+    cardPages.textContent = book.pages;
+    cardReadStatus.textContent = book.readStatus;
+
+    card.appendChild(cardTitle);
+    card.appendChild(cardAuthor);
+    card.appendChild(cardPages);
+    card.appendChild(cardReadStatus);
+
+    document.querySelector("body").appendChild(card);
+  });
+}
+
+addBookToLibrary(
+  "The Oz Principle",
+  "Roger Connors, Tom Smith, Craig Hickman",
+  "232",
+  1
+);
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295", 0);
+
+displayAllBooks();
