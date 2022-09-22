14ed1f6330a1b89c2e11f69b15241196b93f5654
bmilcs <bmilcs@yahoo.com>
2022-09-22 15:54:25 -0400

    Refactor book display method & minor syntax/comment improvements

--- script.js
+++ script.js
@@ -1,16 +1,3 @@
-// ui elements
-
-const bookGrid = document.querySelector(".book-grid"),
-  modalContainer = document.querySelector(".modal-container"),
-  modalWindow = document.querySelector("div.modal"),
-  form = document.getElementById("modal-form"),
-  formTitleBar = document.querySelector(".modal-header"),
-  formSubmitBtn = document.querySelector(".modal button"),
-  titleInput = document.getElementById("book-title"),
-  authorInput = document.getElementById("book-author"),
-  pagesInput = document.getElementById("book-page-count"),
-  readStatusInput = document.getElementById("book-read-status");
-
 // array: stores book objects (details, html elements & methods)
 
 let myLibrary = [];
@@ -22,6 +9,9 @@ function Book(title, author, pages, readStatus) {
   this.author = author;
   this.pages = pages;
   this.readStatus = readStatus;
+  this.id = myLibrary.length;
+
+  this.createCard();
 }
 
 // method: create ui elements for card
@@ -47,11 +37,7 @@ Book.prototype.createCard = function () {
   this.cardPages.textContent = this.pages;
 
   // convert boolean to human readable format & add icon
-  if (this.readStatus === true) {
-    this.setAsRead();
-  } else {
-    this.setAsUnread();
-  }
+  this.readStatus ? this.setAsRead() : this.setAsUnread();
 
   // add font awesome icons: readStatus/edit/delete
   this.cardDeleteIcon.classList.add("fa-solid");
@@ -78,7 +64,7 @@ Book.prototype.createCard = function () {
   this.card.appendChild(this.cardIconContainer);
 
   // display card on page
-  bookGrid.appendChild(this.card);
+  document.querySelector(".book-grid").appendChild(this.card);
 
   // click events for icons on the card: edit/delete/readStatus
   this.cardDeleteIcon.addEventListener("click", () => {
@@ -97,25 +83,6 @@ Book.prototype.createCard = function () {
   this.card.addEventListener("dblclick", () => {
     this.editBook();
   });
-};
-
-// method: launches form to edit book details
-
-Book.prototype.editBook = function () {
-  // update title bar & button label
-  formTitleBar.textContent = `Edit Book #${this.id + 1}`;
-  formSubmitBtn.textContent = "Update Book";
-
-  // populate form with book details
-  titleInput.value = this.title;
-  authorInput.value = this.author;
-  pagesInput.value = this.pages;
-
-  this.readStatus === true
-    ? (readStatusInput.checked = true)
-    : (readStatusInput.checked = false);
-
-  showForm();
 };
 
 // method: remove card element from page & book obj from array
@@ -168,14 +135,47 @@ Book.prototype.toggleReadStatus = function () {
   this.readStatus ? this.setAsRead() : this.setAsUnread();
 };
 
+// global variables
+
+const formTitleBar = document.querySelector(".modal-header"),
+  formSubmitBtn = document.querySelector(".modal button"),
+  titleInput = document.getElementById("book-title"),
+  authorInput = document.getElementById("book-author"),
+  pagesInput = document.getElementById("book-page-count"),
+  readStatusInput = document.getElementById("book-read-status");
+
+// method: launches form to edit book details
+Book.prototype.editBook = function () {
+  // update title bar & button label
+  formTitleBar.textContent = `Edit Book #${this.id + 1}`;
+  formSubmitBtn.textContent = "Update Book";
+
+  // populate form with book details
+  titleInput.value = this.title;
+  authorInput.value = this.author;
+  pagesInput.value = this.pages;
+
+  this.readStatus === true
+    ? (readStatusInput.checked = true)
+    : (readStatusInput.checked = false);
+
+  showForm();
+};
+
 // method: update book details from edit form pop-up
 
 Book.prototype.updateBook = function () {
-  // update card html elements & book object from the form values
-  this.cardTitle.textContent = this.title = titleInput.value;
-  this.cardAuthor.textContent = this.author = authorInput.value;
-  this.cardPages.textContent = this.pages = pagesInput.value;
+  // update book object from the form values
+  this.title = titleInput.value;
+  this.author = authorInput.value;
+  this.pages = pagesInput.value;
 
+  // update elements on page
+  this.cardTitle.textContent = titleInput.value;
+  this.cardAuthor.textContent = authorInput.value;
+  this.cardPages.textContent = pagesInput.value;
+
+  // update readStatus: book object, font awesome icon & label
   if (readStatusInput.checked) {
     this.readStatus = true;
     this.setAsRead();
@@ -191,24 +191,28 @@ function addBook(title, author, pages, readStatus) {
   myLibrary.push(new Book(title, author, pages, readStatus));
 }
 
-// function: called when a new book is added
+// functions: form related
 
-function displayAllBooks() {
-  myLibrary.forEach((book, i) => {
-    // if card exists, skip iteration
-    if (book.existsOnPage) return;
+const modalContainer = document.querySelector(".modal-container");
 
-    // prevent from being duplicated on future iterations
-    book.existsOnPage = 1;
+function hideForm() {
+  // animate: fade-out effect
+  modalContainer.classList.add("hidden");
+  setTimeout(() => {
+    modalContainer.style.display = "none";
+  }, 200);
 
-    // set id so "edit form" can find book within myLibrary array
-    book.id = i;
-
-    book.createCard();
-  });
+  clearForm();
 }
 
-// functions: form related
+function showForm() {
+  modalContainer.style.display = "block";
+  // animate: fade-in effect was not working without this timeout
+  setTimeout(() => {
+    modalContainer.classList.remove("hidden");
+  }, 1);
+  titleInput.focus();
+}
 
 function clearForm() {
   titleInput.value = "";
@@ -217,27 +221,9 @@ function clearForm() {
   readStatusInput.checked = false;
 }
 
-function hideForm() {
-  // animate: fade out effect
-  modalContainer.classList.add("hidden");
-  setTimeout(() => {
-    modalContainer.style.display = "none";
-  }, 200);
-  clearForm();
-}
-
-function showForm() {
-  modalContainer.style.display = "block";
-  // work-around: fade-in effect was not working without this timeout
-  setTimeout(() => {
-    modalContainer.classList.remove("hidden");
-  }, 1);
-  titleInput.focus();
-}
-
 // form submission: called on add/edit book
 
-form.addEventListener("submit", (e) => {
+document.getElementById("modal-form").addEventListener("submit", (e) => {
   // prevent form submission
   e.preventDefault();
 
@@ -249,7 +235,6 @@ form.addEventListener("submit", (e) => {
       pagesInput.value,
       readStatusInput.checked
     );
-    displayAllBooks();
   } else {
     // edit an existing book
     // get book's id within the myLibrary object via titlebar of form
@@ -260,7 +245,7 @@ form.addEventListener("submit", (e) => {
   hideForm();
 });
 
-// header: add book button
+// 'add book' button (page header)
 
 document.querySelector(".btn-add-book").addEventListener("click", () => {
   formTitleBar.textContent = "Add Book to Library";
@@ -280,13 +265,15 @@ window.addEventListener("keydown", (e) => {
   if (e.key === "Escape") hideForm();
 });
 
-// close form: clicking outside of the form
+// close form: click on dark overlay / outside of the form
 
 modalContainer.addEventListener("click", () => hideForm());
 
-// prevent hiding the form when the form window is clicked
+// prevent closing form: when the form itself is clicked
 
-modalWindow.addEventListener("click", (e) => e.stopPropagation());
+document
+  .querySelector("div.modal")
+  .addEventListener("click", (e) => e.stopPropagation());
 
 // sample data
 
@@ -313,5 +300,3 @@ addBook("How to Win Friends & Influence People", "Dale Carnegie", 288, true);
 addBook("The Total Money Makeover", "Dave Ramsey", 237, true);
 
 addBook("Financial Peace", "Dave Ramsey", 288, true);
-
-displayAllBooks();
